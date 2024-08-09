73beb43693f121b1d8ea49c46d76a793b6336e39
Sean <tweestidbunny@gmail.com>
2024-08-08 21:15:46 -0400

    Add ability to change amount of divs, up to 100

--- resources/script.js
+++ resources/script.js
@@ -1,16 +1,18 @@
 const uiWindow = document.querySelector('#window');
 
-for (let i = 0; i < 16; i++) {
-  const column = document.createElement('div');
-  column.classList.add('column');
-  for (let j = 0; j < 16; j++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    row.addEventListener('mouseover', () => row.style.backgroundColor = `#${bgColorChange()}`); //'#f90');
-    // row.addEventListener('click', e => console.log(e));
-    column.appendChild(row);
+function changeSize(size) {
+  uiWindow.textContent = '';
+  for (let i = 0; i < size; i++) {
+    const column = document.createElement('div');
+    column.classList.add('column');
+    for (let j = 0; j < size; j++) {
+      const row = document.createElement('div');
+      row.classList.add('row');
+      row.addEventListener('mouseover', () => row.style.backgroundColor = `#${bgColorChange()}`);
+      column.appendChild(row);
+    }
+    uiWindow.appendChild(column);
   }
-  uiWindow.appendChild(column);
 }
 
 function bgColorChange() {
@@ -19,21 +21,8 @@ function bgColorChange() {
   for (let i = 0; i < 6; i++) {
     colorHex += colorArr[Math.floor(Math.random() * 16)];
   }
-  // console.log(colorHex)
   return colorHex;
 }
-
-// bgColorChange();
-
-// const rowDivs = document.querySelectorAll('.row');
 const btn = document.querySelector('button');
 
-// rowDivs.forEach(div => {
-//   div.addEventListener('mouseover', () => div.classList.add('bgColorChange'));
-//   div.addEventListener('click', e => console.log(e));
-// })
-
-btn.addEventListener('click', () => {
-  const root = document.querySelector(':root');
-  root.style.setProperty('--colorChange', '#003049')
-})
+btn.addEventListener('click', () => changeSize(+prompt('Size below 100: ')))
